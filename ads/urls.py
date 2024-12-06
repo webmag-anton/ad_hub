f179e7cbcca23018351a5c1fa98984f542d82a52
Anton Byshok <anton.byshok@gmail.com>
2024-12-06 01:41:04 +0000

    Add user ads page

--- ads/urls.py
+++ ads/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('my_ads', views.my_ads, name='my_ads'),   
-    path('<slug:slug>/', views.get_ad, name='ad')
+    path('my_ads', views.my_ads, name='my_ads'),  
+    path('user/<int:id>', views.user_ads, name='user_ads'), 
+    path('<slug:slug>/', views.get_ad, name='ad'),
 ]
--- ads/views.py
+++ ads/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Ad
 from .models import AdImage
+from users.models import User
 
 
 @login_required
@@ -14,6 +15,19 @@ def my_ads(request):
     }
 
     return render(request, 'ads/my_ads.html', context)
+
+
+def user_ads(request, id):
+    user = get_object_or_404(User, id=id)
+    userAds = Ad.objects.filter(user=user).prefetch_related('images')
+    userAds_count = userAds.count()
+    context = {
+        'user': user,
+        'userAds': userAds, 
+        'userAds_count': userAds_count
+    }
+
+    return render(request, 'ads/user_ads.html', context)    
 
 
 def get_ad(request, slug):
